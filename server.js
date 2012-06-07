4dda7127447f451ddd32b30198d7144cb5f73ecf
Tommy Bergeron <tomshreds@me.com>
2012-06-07 15:18:49 -0400

    getting interesting

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
-var http = require('http');
+var destinationPort = 3000;
 
-http.createServer(function (req, res) {
+var http = require('http'),
+	url = require('url');
 
+http.createServer(function(req, res) {
+	console.log("\n\n");
+	console.log("Received request.");
+
+	var method = req.method,
+		hostName = req.headers.host,
+		uriInfos = url.parse(req.url, true);
+
+	// todo : set status code
+	// todo : set headers for response
+	// todo : write data
+
+	var options = {
+		host: hostName,
+		port: destinationPort,
+		path: req.url,
+		method: method
+	};
+
+	console.log(options);
+
+	var proxiedRequest = http.request(options, function(proxiedResponse) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(proxiedResponse.headers));
+
+		proxiedResponse.setEncoding('utf8');
+
+		res.writeHead(res.statusCode, { 'Content-Type': proxiedResponse.headers["content-type"] });
+
+		proxiedResponse.on('data', function(chunk) {
+			res.write(chunk);
+			res.end();
+	
+			console.log("\n\n");
+		});
+
+	});
 }).listen(80);
 
 console.log('Server is started.');
